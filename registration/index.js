99f4805a5243d806599f5f7c3f57d0527d5893df
kalyankasi <86098196+Kalyan1234a@users.noreply.github.com>
2021-11-14 20:59:16 +0530

    Add files via upload

---
+++ registration/index.js
@@ -0,0 +1,19 @@
+function func(id) {
+
+    
+    let name = prompt('Enter your name : ','Your Name')
+    document.getElementById('fullName').value = name
+    
+  
+    
+    let std = prompt('Enter your standard', 10)
+    document.getElementById('standard').value = std
+    
+
+  
+    let rollNo = prompt('Enter your phone_no', 000000000)
+    document.getElementById('RollNumber').value = rollNo
+    
+
+
+}
